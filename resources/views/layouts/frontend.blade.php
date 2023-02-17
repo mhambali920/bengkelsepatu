ecaa09f1661104eaaaf7bb757bb4fb7548ea040e
mhambali920 <mhambali920@gmail.com>
2023-02-17 10:12:19 +0700

    ok

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8" />
-  <title>LuxSpace</title>
+  <title>IKKE Konveksi - Produsen Sepatu Bayi dan Balita</title>
   <meta name="description" content="" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
@@ -12,14 +12,9 @@
   <meta property="og:url" content="" />
   <meta property="og:image" content="" />
 
-  <link rel="manifest" href="site.webmanifest" />
+  {{--
+  <link rel="manifest" href="site.webmanifest" /> --}}
   <link rel="apple-touch-icon" href="{{ url('/frontend/images/content/favicon.png') }}" />
-
-  <!-- Place favicon.ico in the root directory -->
-  {{--
-  <link rel="stylesheet" href="{{ url('/frontend/css/main.css') }}" /> --}}
-  {{--
-  <link href="{{ url('/frontend/css/app.minify.css') }}" rel="stylesheet" /> --}}
 
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   <link rel="icon" href="{{ url('/frontend/images/content/favicon.png') }}" />
@@ -29,36 +24,54 @@
 
   <meta name="theme-color" content="#000" />
   <link rel="icon" href="favicon.ico" />
-
-  {{-- <style>
-    .owl-stage-outer {
-      position: relative !important;
+  <script src="{{ url('/frontend/js/jquery-3.6.3.min.js') }}"></script>
+  <script src="{{ url('/frontend/js/owl.carousel.min.js') }}"></script>
+  <style>
+    #carousel1 .owl-item {
+      width: 300px;
+      height: 386px;
     }
 
-    .owl-nav button[type="button"] {
-      font-size: 30px;
-      position: absolute;
-      left: 0;
-      top: 45% !important;
-      color: #252525;
-      width: 50px;
+    .owl-carousel .owl-nav button {
+      background-color: #fff;
+      border: 1px solid #ddd;
+      border-radius: 50%;
+      color: #999;
+      font-size: 18px;
       height: 50px;
-      border-radius: 50% !important;
+      margin: 0 10px;
+      outline: none;
+      width: 50px;
     }
 
-    .owl-nav button[type="button"]:hover {
-      color: #dfad51;
+    .owl-carousel .owl-nav button:hover {
+      background-color: #ddd;
+      border-color: #999;
+      color: #333;
     }
 
-    .owl-nav button[type="button"].owl-next {
-      left: auto;
+    .owl-carousel .owl-nav button span {
+      font-size: 18px;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+
+    .owl-carousel .owl-nav button.owl-prev {
+      position: absolute;
+      top: 50%;
+      left: 0;
+      transform: translate(0, -50%);
+    }
+
+    .owl-carousel .owl-nav button.owl-next {
+      position: absolute;
+      top: 50%;
       right: 0;
+      transform: translate(0, -50%);
     }
-
-    .owl-nav button[type="button"] span {
-      font-size: 30px
-    }
-  </style> --}}
+  </style>
 </head>
 
 <body>
@@ -69,15 +82,6 @@
   @yield('content')
 
   @include('components.frontend.footer')
-
-  <!-- START: LOAD SVG -->
-  <!-- <svg width="23" height="26" class="hidden" id="icon-play">
-      <path
-        d="M21 9.536c2.667 1.54 2.667 5.39 0 6.928l-15 8.66c-2.667 1.54-6-.385-6-3.464V4.34C0 1.26 3.333-.664 6 .876l15 8.66z"
-        fill="#fff"
-      />
-    </svg> -->
-  <!-- END: LOAD SVG  -->
 
   <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
   <script>
@@ -92,26 +96,26 @@
       ga("send", "pageview");
   </script>
   <script src="https://www.google-analytics.com/analytics.js" async></script>
+
   <script src="{{ url('/frontend/js/app.js') }}"></script>
 
-  <script src="{{ url('/frontend/js/jquery-3.6.3.min.js') }}"></script>
-  <script src="{{ url('/frontend/js/owl.carousel.min.js') }}"></script>
+
   <script>
     $(document).ready(function(){
       $('.owl-carousel').owlCarousel({
     loop:true,
     autoplay: true,
     margin:10,
-    // nav:true,
+    nav:true,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:3
+            items:2
         },
         1000:{
-            items:5
+            items:4
         }
     }
 })
